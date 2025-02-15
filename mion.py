3df79017e457a77b43091fea98047034ac615f7d
JuanD508 <dinosaurioguerrillero@gmail.com>
2025-02-15 10:58:45 -0500

    Update mion.py
    En esta nueva versión lo que hice fue añadirle una pregunta en la que el usuario nos indique cuantas contraseñas quiere crear, luego un bucle for que sea para cada contraseña, dentro de esta puse la pregunta de la longitud de la contraseña, y que imprimiera el numero de la contraseña que pidió, y por ultimo puse el código para generar la contraseña dentro de este

--- mion.py
+++ mion.py
@@ -1,7 +1,11 @@
-import random
+import random 
 caract ="+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-long = int(input("Digite la longitud de la contraseña"))
+canti = int(input("Digita cuantas contraseñas quieres crear / "))
 password = ""
-for i in range(long):
-    password += random.choice(caract)
-print(password)
+for i in range(canti):
+    count = i = i + 1
+    long = int(input("Digite la longitud de la contraseña / "))
+    print("contraseña", count)
+    for i in range(long):
+        password += random.choice(caract)
+    print(password)
